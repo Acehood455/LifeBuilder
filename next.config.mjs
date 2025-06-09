44574ffb74ccad964952fe1d021cf1bae04f18d5
Acehood455 <acetaiwo8@gmail.com>
2025-06-09 11:59:36 +0000

    server accessibility fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {hostname: "res.cloudinary.com"}
-        ]
-    },
-    experimental: {
-     serverActions: {
-      allowedHosts: [
+  images: {
+    remotePatterns: [
+      { hostname: "res.cloudinary.com" },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
         "localhost:3000",
-        "https://laughing-bassoon-6vwgppwx94phg54-3000.app.github.dev/",
+        "laughing-bassoon-6vwgppwx94phg54-3000.app.github.dev",
       ],
+      // optional:
+      // allowedForwardedHosts: ["localhost:3000", "laughing-bassoon-6vwgppwx94phg54-3000.app.github.dev"],
     },
   },
 };
